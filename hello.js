b225c77f9b7af70383793d2d215a7a90ea2d64d8
Vikhyatha <vikhyathapuram2006@gmail.com>
2026-01-30 08:01:24 +0000

    commit1

---
+++ hello.js
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Hello World</title>
+    </head>
+    <body>
+        <h2 id="msg"></h2>
+        <script>
+            document.getElementById("msg").innerHTML="Hello World";
+        </script>
+    </body>
+</html>
