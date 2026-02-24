df950cb7e0a4e7ed6e4bfe6e0592e67b9b8f5660
Vikhyatha <vikhyathapuram2006@gmail.com>
2026-02-24 13:26:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,8 +3,6 @@ public class Hello
     public static void main(String[] args)
     {
         System.out.println("Hello world");
-        System.out.println("Hello Manu");
-          System.out.println("Hello Man");
-        System.out.println("Hello again");
+        System.out.println("Hello");
     }
 }
